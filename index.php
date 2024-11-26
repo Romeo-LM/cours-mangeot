878b37c9d6b7d0e5d1c2c91f6edb30747bbad7ed
Roméo <romeolm2005@gmail.com>
2024-11-26 16:08:58 +0100

    modif index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>bouya</title>
+</head>
+<body>
+<h1> fab le gros gay qui joue a pokemon</h1>
+    <?php include("menu.php");?>
+</body>
+</html>
